f449872f8a2d4c0666e79406aae6d300988f6ba9
Aurèle Oulès <aurele@oules.com>
2018-05-27 14:25:33 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# YouTube Personal Statistics
+
+## How to use
+* `$ yarn` or `$ npm install` to install dependencies
+* Create a `config.json` using the `config.json.sample` file. Create a **Google API Key** [here](https://console.developers.google.com/apis/dashboard). You need to active the **YouTube API V3** [here](https://console.developers.google.com/apis/library).
+* Download your **Personal Google Data** [here](https://takeout.google.com/settings/takeout/downloads).
+    - Click on **Create an archive**
+    - Only select **My Activity**
+    - Select data: **YouTube**
+    - Click on next
+    - Click on **Create an archive**
+    - Download your archive
+    - Find the file `My Activity.html` in `Takout/My Activity/YouTube/`
+    - Move it to this folder and rename it activity.html
+* Run using `$ node index.js`. This might take a while.
+
+## How does it work
+* The script read and fetch every links of `activity.html`.
+* It will next detect if the link is a video.
+* A request containing 50 videos will be made because the YouTube API V3 limits the amount of videos to be 50 per request.
+* For each video informations, the duration given by the YouTube API V3 will be converted to a human readable duration.
+* The total amount of time spent on YouTube watching videos will be printed out.
+
+# Author
+[Aurèle Oulès](http://aurele.oules.com)
---
+++ index.js
@@ -0,0 +1,117 @@
+const fs = require("fs");
+const cheerio = require('cheerio');
+const async = require("async");
+const axios = require("axios");
+const ProgressBar = require('progress');
+
+const config = require("./config.json");
+const API_ENDPOINT = "https://www.googleapis.com/youtube/v3";
+const FILE_LOCATION = "./activity.html";
+
+init();
+function init() {
+    clear();
+    console.log("Reading file: " + FILE_LOCATION);
+    fs.readFile(FILE_LOCATION, function (err, html) {
+        console.log("File successfuly read.");
+        if (err) {
+            throw err;
+        } else {
+            console.log("Fetching links...");
+            const ids = fetchIds(html); //split because youtube v3 limits 50 ids per request
+            console.log("Links fetched. (" + ids.length + ")");
+            getTotalDuration(ids);
+        }
+    });
+}
+
+function fetchIds(document) {
+    const $ = cheerio.load(document.toString());
+    const links = $('a');
+    const validUrls = [];
+
+    for(let i = 0; i < links.length; i++) {
+        const link = links[i];
+        const href = link.attribs.href;
+        if (href.includes("https://www.youtube.com/watch?v=")) {
+            validUrls.push(href.replace("https://www.youtube.com/watch?v=", ""));
+        }
+    }
+    return validUrls;
+}
+
+function convert_time(duration) {
+    var a = duration.match(/\d+/g);
+
+    if (duration.indexOf('M') >= 0 && duration.indexOf('H') == -1 && duration.indexOf('S') == -1) {
+        a = [0, a[0], 0];
+    }
+
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1) {
+        a = [a[0], 0, a[1]];
+    }
+    if (duration.indexOf('H') >= 0 && duration.indexOf('M') == -1 && duration.indexOf('S') == -1) {
+        a = [a[0], 0, 0];
+    }
+
+    duration = 0;
+
+    if (a.length == 3) {
+        duration = duration + parseInt(a[0]) * 3600;
+        duration = duration + parseInt(a[1]) * 60;
+        duration = duration + parseInt(a[2]);
+    }
+
+    if (a.length == 2) {
+        duration = duration + parseInt(a[0]) * 60;
+        duration = duration + parseInt(a[1]);
+    }
+
+    if (a.length == 1) {
+        duration = duration + parseInt(a[0]);
+    }
+    return duration
+}
+
+function getTotalDuration(ids) {
+    const totalLength = ids.length;
+    const idsArray = splitIds(ids, 50);
+
+    let totalDuration = 0;
+    let count = 0;
+    const bar = new ProgressBar("Analysing [:bar] :current/:total :percent", {total: totalLength});
+    async.eachSeries(idsArray, (list, cb) => {
+        axios.get(API_ENDPOINT + "/videos", {
+            params: {
+                part: "contentDetails",
+                id: list.join(","),
+                key: config.google_api_key
+            }
+        }).then(response => {
+            if(response) {
+                const videos = response.data.items;
+                for(let i = 0; i < videos.length; i++) {
+                    totalDuration += convert_time(videos[i].contentDetails.duration);
+                }
+                bar.tick(list.length);
+                count += list.length;
+                cb();
+            }
+        }).catch(err => {
+            if(err) console.log(err);
+        });
+    }, () => {
+        console.log("\nTotal duration: " + totalDuration / 3600 + " hours");
+        console.log("Videos: " + totalLength);
+    });
+}
+
+function splitIds(ids, length) {
+    let chunk;
+    let array = [];
+    while (ids.length > 0) {
+        chunk = ids.splice(0, length)
+        array.push(chunk);
+    }
+    return array;
+}
